c3222102c7ca3fc6d51e11efe77d16251af52c08
arc680 <maru.nslab@gmail.com>
2013-01-04 00:21:04 +0900

    修正したヾ(＠⌒ー⌒＠)ノ

--- sub_parts_client_cfg.rb
+++ sub_parts_client_cfg.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 Plugin::create(:sub_parts_client_cfg) do
   settings 'sub_parts_client' do
-    adjustment "margin", :user_margin, 0, 3600
+    adjustment "margin", :user_margin, 0, 100
   end
 end
